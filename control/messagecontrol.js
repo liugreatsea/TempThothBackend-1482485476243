c2664c8cfa1023039c65d5ddb0bb330895d1e444
Derek <liudehai@cn.ibm.com>
2017-06-10 23:02:43 +0800

    add discovery query service

--- control/messagecontrol.js
+++ control/messagecontrol.js
@@ -2,11 +2,14 @@
 'use strict';
 
 var watson = require( 'watson-developer-cloud' );  // watson sdk
+var localConfig = require('../config');
+// var localConfig_workspace_id = localConfig.conversation.workspace_id;
+var localConfig_access_key = localConfig.access_key;
 
 var CONVERSATION_SERVICE = 'conversation';
 var CONVERSATION_ACCESS_ERROR = 'Sorry, you have no authority to use this conversation.';
 
-function getConversationCredential() {
+function getConversationConfig() {
     if (process.env.VCAP_SERVICES) {
         var services = JSON.parse(process.env.VCAP_SERVICES);
         for (var service_name in services) {
@@ -19,65 +22,62 @@ function getConversationCredential() {
                 };
             }
         }
+    }else{
+        return {
+            url: localConfig.conversation.url,
+            username: localConfig.conversation.username,
+            password: localConfig.conversation.password,
+            workspace_id:localConfig.conversation.workspace_id
+        };
     }
     return {};
 };
 
 // Create the service wrapper
-var credential = getConversationCredential();
+var cs_config = getConversationConfig();
 
 var conversation = watson.conversation( {
-    url: credential.url,
-    username: credential.username,
-    password: credential.password,
+    url: cs_config.url,
+    username: cs_config.username,
+    password: cs_config.password,
     version_date:'2016-07-11',
     version: 'v1'
 } );
 
 
+
 /**
  * Updates the response text using the intent confidence
- * @param  {Object} input The request to the Conversation service
+ * @param  {Object} payload The request to the Conversation service
  * @param  {Object} response The response from the Conversation service
  * @return {Object}          The response with the updated message
  */
-function updateMessage(input, response) {
+function updateMessage(payload, response) {
     var responseText = null;
     var id = null;
 
     if ( !response.output ) {
         response.output = {};
     } else {
-        if ( response.output.api ) {
-            //TODO call REST API, Dummy source first
-            var specialContent ={};
-            specialContent.data=[
-                {
-                    'Name':'',
-                    'Info':'',
-                    'MimeList':[
-                        {'Type':'vedio','URL':'http://www.runoob.com/try/demo_source/movie.mp4'},
-                        {'Type':'graphic','URL':'http://www.smbc.co.jp/kojin/resources/images/index_logo03.jpg'}
-                    ]
-                }
-            ];
-            response.output.specialContent = specialContent;
+
+        if ( response.intents && response.intents[0] ) {
+            var intent = response.intents[0];
+            if ( intent.confidence >= 0.75 ) {
+                responseText = 'I understood your intent was ' + intent.intent;
+            } else if ( intent.confidence >= 0.5 ) {
+                responseText = 'I think your intent was ' + intent.intent;
+            } else {
+                responseText = 'I did not understand your intent';
+            }
+
+            response.output.text = responseText;
+
         }
         return response;
-    }
-    if ( response.intents && response.intents[0] ) {
-        var intent = response.intents[0];
-        if ( intent.confidence >= 0.75 ) {
-            responseText = 'I understood your intent was ' + intent.intent;
-        } else if ( intent.confidence >= 0.5 ) {
-            responseText = 'I think your intent was ' + intent.intent;
-        } else {
-            responseText = 'I did not understand your intent';
-        }
-    }
-    response.output.text = responseText;
 
-    return response;
+    }
+
+
 }
 
 //
@@ -85,7 +85,7 @@ var messageControl = {
 
     message: function messageControl(req, res) {
 
-        var workspace = process.env.conversation_workspace_id;
+        var workspace = process.env.conversation_workspace_id||cs_config.workspace_id;
 
         if ( !workspace || workspace === '' ) {
             return res.json( {
@@ -102,12 +102,11 @@ var messageControl = {
             context: {},
             input: {},
         };
+        var accessKey = process.env.conversation_access_key||localConfig_access_key;
         if ( req.body ) {
-            /*console.log("*********************req body accessKey:" + req.body.accessKey);
-            console.log("*********************access Key:" + process.env.conversation_access_key);
-            if ( req.body.accessKey!== process.env.conversation_access_key) {
+            if ( req.body.accessKey!== accessKey) {
                 return res.status( 403 ).json( { error: CONVERSATION_ACCESS_ERROR } );
-            }*/
+            }
             if ( req.body.input ) {
                 payload.input = req.body.input;
             }
@@ -121,7 +120,35 @@ var messageControl = {
             if ( err ) {
                 return res.status( err.code || 500 ).json( err );
             }
-            return res.json( updateMessage( payload, data ) );
+            var response = updateMessage( payload, data );
+
+
+            if ( response.output.discovery_search ) {
+
+                var DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
+                var discovery = new DiscoveryV1({
+                    username: localConfig.discovery.username,
+                    password: localConfig.discovery.password,
+                    version_date: '2016-12-01'
+                });
+
+                var environment_id = localConfig.discovery_collection_untrained.environment_id;
+                var collection_id = localConfig.discovery_collection_untrained.collection_id;
+                var query_string = payload.input.text;
+
+                discovery.query({environment_id:environment_id, collection_id:collection_id,query:query_string}, function(error, data) {
+                    if (data.results && data.results.length>0) {
+                        response.output.text = data.results[0].text;
+                    }
+                    return res.json(response);
+
+                });
+
+            } else {
+                return res.json(response);;
+            }
+
+
         } );
     }
 
